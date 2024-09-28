a26200eb254ad70325cec639fc30bf023a814d3d
Brb93 <bryanbrik@gmail.com>
2024-09-28 15:33:04 +0200

    etape 5 : l'opération

--- main.c
+++ main.c
@@ -7,46 +7,78 @@ int main()
 	int nombre_un;
 	int nombre_second;
 	char operateur;
-	double resultat;
 
 	printf("Bonjour, veuillez rentrer un opérateur parmis la liste suivante : '+' , '-', '*', '/', '%%'.\n");
-	scanf("%c", &operateur);
-
-	printf("Veuillez rentrer votre premier nombre : \n");
-	scanf("%d", &nombre_un);
-
-	printf("Veuillez rentrer votre second nombre : \n");
-	scanf("%d", &nombre_second);
+	scanf(" %c", &operateur);
 
 	if (operateur == '+')
 	{
-		printf("addition \n");
-	}
+		int resultat;
+		printf("Veuillez rentrer votre premier nombre : \n");
+		scanf("%d", &nombre_un);
+		printf("Veuillez rentrer votre second nombre : \n");
+		scanf("%d", &nombre_second);
+		resultat = nombre_un + nombre_second;
+		printf("Le résultat de votre addition est : %d\n", resultat);
+
+	} 
 
 	else if (operateur == '-')
 	{
+		int resultat;
+		printf("Veuillez rentrer votre premier nombre : \n");
+		scanf("%d", &nombre_un);
+		printf("Veuillez rentrer votre second nombre : \n");
+		scanf("%d", &nombre_second);
+		resultat = nombre_un - nombre_second;
 		printf("soustraction \n");
 	}
 
 	else if (operateur == '*')
 	{
+		double resultat;
+		printf("Veuillez rentrer votre premier nombre : \n");
+		scanf("%d", &nombre_un);
+		printf("Veuillez rentrer votre second nombre : \n");
+		scanf("%d", &nombre_second);
+		resultat = nombre_un * nombre_second;
 		printf("multiplication \n");
 	}
 
 	else if (operateur == '/')
 	{
-		printf("division \n");
+		double resultat;
+		printf("Veuillez rentrer votre premier nombre : \n");
+		scanf("%d", &nombre_un);
+		printf("Veuillez rentrer votre second nombre : \n");
+		scanf("%d", &nombre_second);
+		if (nombre_second == 0)
+		{
+			printf("Il est impossible de diviser par zéro!\n");
+		}
+		else
+		{
+			resultat = nombre_un % nombre_second;
+			printf("%2.f \n", resultat);
+		}	
+	}
+	else if (operateur == '%')
+	{	
+		int resultat;
+		printf("Veuillez rentrer votre premier nombre : \n");
+		scanf("%d", &nombre_un);
+		printf("Veuillez rentrer votre second nombre : \n");
+		scanf("%d", &nombre_second);
+		resultat = nombre_un % nombre_second;
+		printf("modulo \n");
+
 	}
 
-	else if (operateur == '%')
-	{
-		printf("modulo \n");
-	}
-	
 	else
 	{
-		printf("Opérateur non reconnu \n");
+		printf("Opérateur hs \n");
 	}
+
 
 
 
